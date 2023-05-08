2c55467694ae1f1fa6a0bff88dd56ec40a95a5c1
ShaikSulthana <ssulthanajaan789@gmail.com>
2023-05-08 11:55:44 +0530

    not wish

--- asma programs/Division.java
+++
@@ -1,8 +0,0 @@
-class Division
-{
-    public static void main(String[] args)
-{
-    int n=10%3;
-    System.out.println(n);
-}
-}
--- asma programs/Ternaryg2.java
+++
@@ -1,9 +0,0 @@
-class Ternaryg2 {
-    public static void main(String[] args) 
-    {
-        int a=20;
-        int b=30;
-        String result=a>b?a+"is greatest":b+"is greatest";
-        System.out.println(result);
-    }
-}
